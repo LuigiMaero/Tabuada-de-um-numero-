41f906e934c979140277db98a81820f8bad67a3b
hydrargyrus <50925295+LuigiMaero@users.noreply.github.com>
2019-05-22 15:21:02 -0300

    Pasta
    Pasta do programa completa.

---
+++ tabuadafor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuadafor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numdig;
+
+            Console.Write("Digite um número: ");
+            numdig = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numdig * 10; i++)
+            {
+                if (i % numdig == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
